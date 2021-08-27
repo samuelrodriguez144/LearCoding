1b9d2909d9ac4839c9364086e478e27dff405d16
Samuel Rodriguez <samuelfrodriguezjr@gmail.com>
2021-08-28 06:00:03 +0800

    Add retrofit dependency

--- app/src/main/java/com/guren/learncoding/base/BaseApplication.kt
+++ app/src/main/java/com/guren/learncoding/base/BaseApplication.kt
@@ -4,7 +4,6 @@ import android.app.Application
 import android.content.Context
 import com.facebook.stetho.Stetho
 
-
 class BaseApplication: Application() {
 
     init {
--- app/src/main/java/com/guren/learncoding/util/CustomAlerts.kt
+++ app/src/main/java/com/guren/learncoding/util/CustomAlerts.kt
@@ -5,14 +5,14 @@ import android.widget.Toast
 import com.google.android.material.snackbar.Snackbar
 import com.guren.learncoding.base.BaseApplication
 
-public class CustomAlerts {
+class CustomAlerts {
 
     fun showToast(message:String){
         Toast.makeText(BaseApplication.applicationContext(), message, Toast.LENGTH_SHORT).show()
     }
 
     fun showSnackBar(view: View, message: String){
-        val snack = Snackbar.make(view, message, Snackbar.LENGTH_INDEFINITE)
-        snack.show()
+        Snackbar.make(view, message, Snackbar.LENGTH_INDEFINITE).show()
+
     }
 }
